2a421179b08eaa40ab56f194344e227835f806b9
Seungyeon Yoo <y2s9310@gmail.com>
2021-05-19 03:00:12 +0900

    Add: TextInput && TextArea components

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,6 @@
-export { default as InputTags } from './InputTags';
 export * from './OAuthButtons';
+export * from './TextInput';
+export { default as InputTags } from './InputTags';
 export { default as Modal } from './Modal';
 export { default as SearchInputs } from './SearchInputs';
 export { default as Header } from './Header';
