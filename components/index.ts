3dbcbeb36a91696f89289b9c79ba1022030b7f0b
Seungyeon Yoo <y2s9310@gmail.com>
2021-05-18 22:25:08 +0900

    Add: FileInput component

--- components/index.ts
+++ components/index.ts
@@ -3,3 +3,4 @@ export * from './OAuthButtons';
 export { default as Modal } from './Modal';
 export { default as SearchInputs } from './SearchInputs';
 export { default as Header } from './Header';
+export { default as FileInput } from './FileInput';
