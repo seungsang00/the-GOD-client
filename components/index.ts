003db2315ba9ef5e8de508b01ab496bf8d6dea70
Seungyeon Yoo <y2s9310@gmail.com>
2021-05-18 12:35:28 +0900

    Remove: non-existent module

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,4 @@
 export { default as InputTags } from './InputTags';
-export { default as Dropdown } from './Dropdown';
 export * from './OAuthButtons';
 export { default as Modal } from './Modal';
 export { default as SearchInput } from './SearchInputs';
