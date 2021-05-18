402d4f255a58001c762b030aecdd5b0aaf910629
Seungyeon Yoo <y2s9310@gmail.com>
2021-05-18 17:24:15 +0900

    Add: Header component

---
+++ components/Header/Header.style.ts
@@ -0,0 +1,14 @@
+import styled from '@styles/themed-components';
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 ${({ theme }) => theme.space.sm};
+  ${({ theme }) => theme.concept.glassmorphism}
+  position: sticky;
+  top: 0;
+  left: 0;
+`;
--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,4 @@ export { default as InputTags } from './InputTags';
 export * from './OAuthButtons';
 export { default as Modal } from './Modal';
 export { default as SearchInputs } from './SearchInputs';
+export { default as Header } from './Header';
