1344d8ccf336d0142666775590b4e5484e6a4d1e
Seungyeon Yoo <y2s9310@gmail.com>
2021-05-24 10:59:56 +0900

    Update: Layout component

--- layouts/Layout.tsx
+++ layouts/Layout.tsx
@@ -1,7 +1,10 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, MouseEvent } from 'react';
 import Head from 'next/head';
 import { Avatar, Header } from '@components';
 import { CommonLayoutStyle } from './layouts.style';
+import { useRouter } from 'next/dist/client/router';
+import useModal from 'hooks/useModal';
+import { AuthModal } from 'containers/auth';
 
 type Props = {
   children?: ReactNode;
@@ -14,12 +17,22 @@ const Layout = ({
   title = 'FansSum | 팬심이 모여 문화가 되다',
   footer,
 }: Props) => {
-  // const profileImage = 'https://bit.ly/3oqUbfM'; // FIXME: 스토어에서 유저의 프로필이미지를 받아와야 합니다
-  const profileImage = undefined;
+  const { isOpen, modalController } = useModal();
+  const router = useRouter();
 
-  const handleNavOpen = () => {
-    // TODO: 메뉴 영역이 보이도록 컨트롤 하는 핸들러 함수 추가
-    console.log('menu open');
+  // FIXME: 스토어에서 유저 정보를 받아와야 합니다
+  const profileImage = undefined;
+  const isLogin = true;
+
+  const handleAvatarClick = (e: MouseEvent) => {
+    // TODO: 로그인 상태가 아니라면 로그인 모달 오픈
+    if (!isLogin) {
+      modalController(e);
+      console.log(`login first!`);
+    } else {
+      console.log('move to mypage!');
+      router.push('/mypage');
+    }
   };
 
   return (
@@ -32,12 +45,16 @@ const Layout = ({
       <Header
         avatar={
           <Avatar
-            profileImage={profileImage ? profileImage : '/images/user.svg'}
-            size={2.5}
-            handler={handleNavOpen}
+            profileImage={
+              profileImage ? profileImage : '/images/avatar_default.jpg'
+            }
+            size={2}
+            title="My Page"
+            handler={handleAvatarClick}
           />
         }
       />
+      <AuthModal isOpen={isOpen} handler={modalController} />
       <CommonLayoutStyle>{children}</CommonLayoutStyle>
       {footer}
     </>
--- layouts/layouts.style.ts
+++ layouts/layouts.style.ts
@@ -4,6 +4,19 @@ export const CommonLayoutStyle = styled.div`
   #mainContent {
     padding: ${({ theme }) => theme.space.lg} 0;
   }
+
+  /* main page */
+  #mainBanner {
+    height: 60vh;
+    padding: ${({ theme }) => theme.space.xxl} 0;
+
+    ${({ theme }) => theme.media.tablet} {
+      padding: ${({ theme }) => theme.space.xl} 0;
+    }
+    ${({ theme }) => theme.media.mobile} {
+      padding: ${({ theme }) => theme.space.lg} 0;
+    }
+  }
 `;
 
 export const MyPageStyle = styled.article`
