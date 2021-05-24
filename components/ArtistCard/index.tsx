75e161c93c02238b9a9a2f54d707864f5ce97c7c
Seungyeon Yoo <y2s9310@gmail.com>
2021-05-24 14:07:46 +0900

    Add: todo comment into ArtistCard component

--- components/ArtistCard/index.tsx
+++ components/ArtistCard/index.tsx
@@ -13,6 +13,9 @@ const ArtistCard = ({
   group,
   profileImage,
 }: ArtistCardProps): ReactElement => {
+  // TODO: 다음 기능이 추가되어야 합니다
+  // 아티스트 언팔로우 기능
+  // 컴포넌트 클릭 시 아티스트 검색 결과 페이지(?)로 이동
   return (
     <CardWrapper id={`artist${id}`}>
       <AvatarContainer>
