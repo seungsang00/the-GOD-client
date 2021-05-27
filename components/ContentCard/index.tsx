4c2f555e7308861854bb9394923abf7da615af39
Seungyeon Yoo <y2s9310@gmail.com>
2021-05-27 16:22:51 +0900

    Update: ContentCard component

--- components/ContentCard/index.tsx
+++ components/ContentCard/index.tsx
@@ -1,20 +1,20 @@
+import { ImagesContainer } from '@components';
 import { Content } from '@interfaces';
 import React from 'react';
 import ContentCardStyle from './ContentCard.style';
 
-const ContentCard = ({ title, description, date, images }: Content) => {
+interface ContentCardProps {
+  isOpen: boolean; // 현재 디테일 영역이 열려있는 컨텐츠id
+  contentData: Content;
+}
+const ContentCard = ({ isOpen, contentData }: ContentCardProps) => {
+  const { title, description, date, images } = contentData;
   return (
     <ContentCardStyle>
       <div>{title}</div>
       <div>{description}</div>
       <div>{date}</div>
-      <div>
-        {images.map((el, i) => {
-          <div key={i}>
-            <img src={el} alt={`${title}-img-${i}`} />
-          </div>;
-        })}
-      </div>
+      {isOpen && <ImagesContainer title={title} images={images} max={3} />}
     </ContentCardStyle>
   );
 };
