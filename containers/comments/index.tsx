79e2bf168c16019993ab147002d6ae8430730c26
Seungyeon Yoo <y2s9310@gmail.com>
2021-06-02 00:17:59 +0900

    Update: content's comments

--- containers/comments/index.tsx
+++ containers/comments/index.tsx
@@ -1,4 +1,5 @@
 import { Comment, CommentInput } from '@components';
+import { sampleCommentsData } from '@utils/sample-data';
 import useTextInput from 'hooks/useTextInput';
 import { createCommentThunk, getCommentListThunk } from 'modules/comment';
 import { RootState } from 'modules/reducer';
@@ -8,41 +9,46 @@ import { useDispatch, useSelector } from 'react-redux';
 
 const Comments = () => {
   const router = useRouter();
+  const dispatch = useDispatch();
+  const { id } = router.query as { id: string };
+  const { inputEvent } = useTextInput('');
+
   const { loading, error, data } = useSelector(
     (state: RootState) => state.comment.list
   );
-  const { id } = router.query as { id: string };
+  const { data: newCommentData } = useSelector(
+    (state: RootState) => state.comment.create
+  );
+
+  const handleSubmitNewComment = () => {
+    const comment = { id, comment: inputEvent.value };
+    dispatch(createCommentThunk(comment));
+  };
+
   useEffect(() => {
-    if (id) getCommentListThunk(id);
+    if (id) dispatch(getCommentListThunk(id));
   }, []);
   useEffect(() => {
     if (error) {
       alert(error);
     }
   }, [error]);
-
-  const { inputEvent } = useTextInput('');
-  const { data: newCommentData } = useSelector(
-    (state: RootState) => state.comment.create
-  );
-  const dispatch = useDispatch();
-  const handleSubmitNewComment = () => {
-    const comment = { contentId: id, comment: inputEvent.value };
-    dispatch(createCommentThunk(comment));
-    // TODO: 댓글추가가 정상적으로 이루어지면 댓글 인풋창 초기화
-    if (newCommentData) {
-      inputEvent.value = '';
-    }
-  };
   useEffect(() => {
-    if (id) getCommentListThunk(id);
+    inputEvent.setValue(''); // 댓글추가가 정상적으로 이루어지면 댓글 인풋창 초기화
+    if (id) dispatch(getCommentListThunk(id));
   }, [newCommentData]);
 
   return (
     <article>
       {loading && <p>loading...</p>}
       <CommentInput handler={handleSubmitNewComment} {...inputEvent} />
-      {data && data.map((comment) => <Comment commentData={comment} />)}
+      {data &&
+        data
+          .sort((a, b) => (b.createdAt > a.createdAt ? 1 : -1))
+          .map((comment) => <Comment commentData={comment} />)}
+      {/* 아래는 삭제되어야 할 부분 (테스트용) */}
+      {/* {!data &&
+        sampleCommentsData.map((comment) => <Comment commentData={comment} />)} */}
     </article>
   );
 };
