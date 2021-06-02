bfff180e28ff4d4c45ffbff20c10ad661d052be6
Seungyeon Yoo <y2s9310@gmail.com>
2021-06-02 16:33:16 +0900

    Update: search form style

--- components/OptionList/OptionList.style.ts
+++ components/OptionList/OptionList.style.ts
@@ -1,10 +1,17 @@
 import styled from '@styles/themed-components';
 
 export const OptionListStyle = styled.ul`
+  width: 100%;
+  max-height: 50vh;
+  display: flex;
+  flex-flow: column wrap;
   padding: ${({ theme }) => theme.space.xs} ${({ theme }) => theme.space.xs};
   border-radius: ${({ theme }) => theme.borderRadius};
   box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
   background-color: #fff;
+  ${({ theme }) => theme.concept.glassmorphism};
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.bgColor};
 
   .option-item {
     cursor: pointer;
@@ -14,6 +21,7 @@ export const OptionListStyle = styled.ul`
   }
   .option-item:hover {
     border-radius: ${({ theme }) => theme.borderRadius};
-    background-color: ${({ theme }) => theme.colors.grey} !important;
+    ${({ theme }) => theme.concept.glassmorphism};
+    border: none;
   }
 `;
