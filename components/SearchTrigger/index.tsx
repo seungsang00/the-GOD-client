b8672f28dcf31307b346bf3c75ec2e0bc007b265
Seungyeon Yoo <y2s9310@gmail.com>
2021-06-02 16:31:30 +0900

    Add: 검색 기능 구현

--- components/SearchTrigger/index.tsx
+++ components/SearchTrigger/index.tsx
@@ -2,7 +2,7 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import moment from 'moment';
 import { useRouter } from 'next/router';
-import { MouseEventHandler } from 'react';
+import { MouseEventHandler, useEffect, useState } from 'react';
 import {
   SearchTriggerWrapper,
   TriggerBg,
@@ -11,32 +11,46 @@ import {
   SearchIcon,
 } from './SearchTrigger.style';
 
+interface QueryData {
+  artist: string;
+  location: string;
+  dateStart: string;
+  dateEnd: string;
+}
+
 const SearchTrigger = ({ handler }: { handler: MouseEventHandler }) => {
   const router = useRouter();
-  // TODO: store에서 검색 쿼리에 대한 데이터를 받아와서 넣어주어야 합니다
-  const searchData = {
-    queryData: {
-      artist: 'ENHYPEN',
-      location: '서울 강남구',
-      dateStart: '2021-05-28',
-      dateEnd: '2021-06-07',
-    },
-  };
-  const { artist, location, dateStart, dateEnd } = searchData.queryData;
+  const [queryData, setQueryData] = useState<QueryData | null>(null);
+  const { pathname, query } = router;
+
+  useEffect(() => {
+    if (pathname === '/search') {
+      const { artist, location, dateStart, dateEnd } = query as {
+        artist: string;
+        location: string;
+        dateStart: string;
+        dateEnd: string;
+      };
+      console.log(artist, location, dateStart, dateEnd);
+      setQueryData({ artist, location, dateStart, dateEnd });
+    }
+  }, []);
 
   return (
     <SearchTriggerWrapper className="search-trigger">
       <TriggerBg className="trigger-bg">
         <Trigger onClick={handler}>
-          {router.pathname === '/' || !searchData.queryData ? (
+          {router.pathname === '/' || !queryData ? (
             <SearchLabel onClick={handler}>검색하기</SearchLabel>
           ) : (
             <>
-              <SearchLabel onClick={handler}>{artist}</SearchLabel>
-              <SearchLabel onClick={handler}>{location}</SearchLabel>
-              <SearchLabel onClick={handler}>{`${moment(dateStart).format(
+              <SearchLabel onClick={handler}>{queryData.artist}</SearchLabel>
+              <SearchLabel onClick={handler}>{queryData.location}</SearchLabel>
+              <SearchLabel onClick={handler}>{`${moment(
+                queryData.dateStart
+              ).format('MM월 DD일')} - ${moment(queryData.dateEnd).format(
                 'MM월 DD일'
-              )} - ${moment(dateEnd).format('MM월 DD일')}`}</SearchLabel>
+              )}`}</SearchLabel>
             </>
           )}
           <SearchIcon>
