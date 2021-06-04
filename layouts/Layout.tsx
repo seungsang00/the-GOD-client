ae63020da6dd7e33da1d72a53448086bd0373434
Seungyeon Yoo <y2s9310@gmail.com>
2021-06-04 11:53:11 +0900

    Fix: Layout component에 회원정보를 불러오는 과정 추가

--- layouts/Layout.tsx
+++ layouts/Layout.tsx
@@ -1,12 +1,11 @@
-import React, { ReactNode, MouseEvent } from 'react';
+import React, { ReactNode, useEffect } from 'react';
 import Head from 'next/head';
 import { Avatar, Header } from '@components';
 import { CommonLayoutStyle } from './layouts.style';
 import { useRouter } from 'next/dist/client/router';
-import useModal from 'hooks/useModal';
-import { AuthModal } from '@containers';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'modules/reducer';
+import { getInfoThunk } from 'modules/user';
 
 type Props = {
   children?: ReactNode;
@@ -19,23 +18,15 @@ const Layout = ({
   title = 'FansSum | 팬심이 모여 문화가 되다',
   footer,
 }: Props) => {
-  const { isOpen, modalController, setIsOpen } = useModal();
   const router = useRouter();
-
-  // FIXME: 스토어에서 유저 정보를 받아와야 합니다
+  const dispatch = useDispatch();
   const { data } = useSelector((state: RootState) => state.user.userProfile);
-
-  const handleAvatarClick = (e: MouseEvent) => {
-    // TODO: 로그인 상태가 아니라면 로그인 모달 오픈
-    if (!data) {
-      modalController(e);
-      console.log(`login first!`);
-    } else {
-      console.log('move to mypage!');
-      router.push('/mypage');
-    }
+  const handleAvatarClick = () => {
+    router.push('/mypage');
   };
-
+  useEffect(() => {
+    if (!data) dispatch(getInfoThunk());
+  }, []);
   return (
     <>
       <Head>
@@ -54,11 +45,6 @@ const Layout = ({
             handler={handleAvatarClick}
           />
         }
-      />
-      <AuthModal
-        isOpen={isOpen}
-        handler={modalController}
-        setIsOpen={setIsOpen}
       />
       <CommonLayoutStyle>{children}</CommonLayoutStyle>
       {footer}
