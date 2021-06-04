ec32fc9271b4003f382959e222bd4b35ed1afb66
Seungyeon Yoo <y2s9310@gmail.com>
2021-06-05 05:43:52 +0900

    Move: formstyle.style.ts into /layout

--- layouts/index.ts
+++ layouts/index.ts
@@ -1,2 +1,4 @@
 export { default as Layout } from './Layout';
 export { default as MyPageLayout } from './MyPageLayout';
+export * from './FormLayout.style';
+export { default as FormStyle } from './formstyle.style';
